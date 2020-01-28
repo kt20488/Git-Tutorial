08453034918f9430f978aa3110b431ddfd5e4dff
kt20488 <60376191+kt20488@users.noreply.github.com>
2020-01-28 15:39:59 -0600

    자바 OOP들어가기전까지. 기본개념 공부들.

---
+++ ClassPart.java
@@ -0,0 +1,14 @@
+package classpart;
+
+public class ClassPart {
+
+	int studentID;
+	String studentName;
+	String address;
+	
+	public void showStudentInfo() {
+		System.out.println(studentName + "," + address);
+	}
+
+}
+
