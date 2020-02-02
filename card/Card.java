4427cc9b08f21e118fd0b31782e5252bf8a107a2
kt20488 <60376191+kt20488@users.noreply.github.com>
2020-02-01 18:36:04 -0600

    2020_1_30

---
+++ card/Card.java
@@ -0,0 +1,17 @@
+package card;
+
+public class Card {
+	
+	//private int Number;
+	
+	Card card = new Card() {};
+	public void getCardNumber() {
+//	Number = CardNumber;
+	}
+	/*public static int CardNumber = 10000;
+
+	public void createCard() {
+		CardNumber ++;
+	}*/
+	
+}
---
+++ card/CardCompany.java
@@ -0,0 +1,17 @@
+package card;
+
+public class CardCompany {
+
+	
+	private static CardCompany instance = new CardCompany();
+	
+	private CardCompany() {}
+	public static CardCompany getInstance() {
+		
+		if(instance == null) {
+			instance = new CardCompany();
+		}
+		return instance;
+		
+	}
+}
