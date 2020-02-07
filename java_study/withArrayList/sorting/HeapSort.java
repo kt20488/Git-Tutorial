477e4ad71cdbb1a8205e96f77e4e2f6da093fb64
kt20488 <60376191+kt20488@users.noreply.github.com>
2020-02-07 10:06:38 -0600

    2020_2_6

---
+++ java_study/withArrayList/sorting/HeapSort.java
@@ -0,0 +1,24 @@
+package sorting;
+
+public class HeapSort implements Sort{
+
+	@Override
+	public void ascending(int[] arr) {
+		System.out.println("HeapSort ascending");		
+	}
+
+	@Override
+	public void descending(int[] arr) {
+		System.out.println("HeapSort descending");		
+	}
+
+	
+	public void description() {
+		Sort.super.description();
+		System.out.println("HeapSort ¿‘¥œ¥Ÿ");
+		
+	}
+	
+	
+	
+}
---
+++ java_study/withArrayList/sorting/SortTest.java
@@ -0,0 +1,37 @@
+package sorting;
+
+import java.io.IOException;
+
+public class SortTest {
+
+	public static void main(String[] args) throws IOException {
+
+		System.out.println("정렬 방식을 선택하세요");
+		System.out.println("B : BubbleSort");
+		System.out.println("Q : QuickSort");
+		System.out.println("H : HeapSort");
+		
+	int ch = System.in.read();	
+	Sort sort = null;
+	
+	if(ch == 'B' || ch =='b') {
+		sort = new BubbleSort();
+	}
+	else if(ch == 'H' || ch =='h') {
+		sort = new HeapSort();
+	}
+	else if(ch == 'Q' || ch =='q') {
+		sort = new QuickSort();
+	}
+	else {
+		System.out.println("지원되지 않는 기능입니다.");
+		return;
+	}
+	int[] arr = new int[0];
+	sort.ascending(arr);
+	sort.descending(arr);
+	sort.description();
+	
+			
+	}
+}
